9383088b78b9823b8796a2f669224397a64beebe
mstonya2023 <latonya.davis08@gmail.com>
2023-10-02 10:14:27 -0700

    Complete index and detail function

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('accounts/signup/', views.signup, name='signup'),
-  path('posts/', views.posts_index, name='index')
+  path('posts/', views.posts_index, name='index'),
+  path('posts/<int:post_id>/', views.posts_detail, name='detail')
 
 ]
