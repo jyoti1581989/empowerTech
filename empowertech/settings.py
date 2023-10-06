8d409a2904b8c485fb7d936be9b012083c2edfc4
kenzatazilabzour <kenzatazi17@gmail.com>
2023-10-06 09:29:16 -0700

    update

--- empowertech/settings.py
+++ empowertech/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-LOGIN_REDIRECT_URL = '/empowerTech/'
+LOGIN_REDIRECT_URL = '/'
 
 # Add this variable to specify where logging out redirects to
 LOGOUT_REDIRECT_URL = '/'
